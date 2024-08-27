ab0a773eba79b4df270fcf3dc4cb0a19c1744ae9
alex <alexandre.urwald@renesas.com>
2024-08-27 17:48:59 +0200

    update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -3,12 +3,18 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 import logging, sys
+from datetime import datetime
+
+# Count all database connections
+connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -31,19 +37,29 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
+#Function that logs messages
+def log_message(msg):
+    app.logger.info('{time} | {message}'.format(
+        time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), message=msg))
+
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        log_message(
+            'Article with id "{id}" does not exist!'.format(id=post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        log_message('Article "{title}" retrieved!'.format(title=post['title']))
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_message('About page rendered!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -86,7 +102,7 @@ def metrics():
     # Response 
     response = app.response_class(
         response=json.dumps({"data": {
-            "db_connection_count": app.config['db_count'], 
+            "db_connection_count": connection_count, 
             "post_count": len(posts)
         }
         }),
